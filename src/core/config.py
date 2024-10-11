803b86aaf7b02d2167857de76aa76fec6ab0be8e
yesandv <grc@protonmail.com>
2024-10-11 19:20:45 +0300

    Add the lifespan

--- src/core/config.py
+++ src/core/config.py
@@ -3,20 +3,15 @@ from logging import config as logging_config
 
 from core.logger import LOGGING
 
-# Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-# Название проекта. Используется в Swagger-документации
-PROJECT_NAME = os.getenv("PROJECT_NAME", "movies")
+PROJECT_NAME = os.getenv("PROJECT_NAME", "filmworks")
 
-# Настройки Redis
 REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
-# Настройки Elasticsearch
-ELASTIC_SCHEME = "http"
+ELASTIC_SCHEME = os.getenv("ELASTIC_SCHEME", "http")
 ELASTIC_HOST = os.getenv("ELASTIC_HOST", "127.0.0.1")
 ELASTIC_PORT = int(os.getenv("ELASTIC_PORT", 9200))
 
-# Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
