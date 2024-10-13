5e3223081b01cfb76f3b47a2e3b2f38ca245eb47
yesandv <grc@protonmail.com>
2024-10-13 22:25:09 +0300

    Update film endpoints

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -15,14 +15,14 @@ router = APIRouter(prefix="/api/v1/films", tags=["FilmService"])
     description="Получение всех фильмов",
 )
 async def get_films(
-        genre: str = None,
+        genre_id: str = None,
         sort: str = "-imdb_rating",
         page_number: int = Query(1),
         page_size: int = Query(50),
         film_service: FilmService = Depends(get_film_service),
 ) -> list[Film]:
     return await film_service.get_films(
-        sort, page_number, page_size, genre_id=genre
+        sort, page_number, page_size, genre_id=genre_id
     )
 
 
@@ -61,3 +61,21 @@ async def get_film_details(
             status_code=HTTPStatus.NOT_FOUND, detail="Film was not found"
         )
     return film
+
+
+@router.get(
+    "/{film_id}/alike",
+    response_model=list[Film],
+    response_model_by_alias=False,
+    description="Похожие фильмы",
+)
+async def get_films_alike(
+        film_id: str, film_service: FilmService = Depends(get_film_service)
+) -> list[Film]:
+    film = await film_service.get_by_id(film_id)
+    films_alike = await film_service.get_by_genres(film.genres)
+    if not films_alike:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="No films alike"
+        )
+    return films_alike
