9b2fb3923105b2c97eaef045ebd9dd91f5d0b385
yesandv <grc@protonmail.com>
2024-10-27 15:12:01 +0200

    Update hosts

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -5,7 +5,11 @@ from elasticsearch import Elasticsearch
 from tests.settings import test_settings
 
 if __name__ == "__main__":
-    es_client = Elasticsearch(hosts=test_settings.es_host)
+    es_client = Elasticsearch(
+        hosts=[
+            f"{test_settings.es_schema}://{test_settings.es_host}:{test_settings.es_port}"
+        ]
+    )
     while True:
         if es_client.ping():
             break
